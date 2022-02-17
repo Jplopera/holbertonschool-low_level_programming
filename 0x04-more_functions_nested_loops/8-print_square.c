ca3fc11ef14daf156805df9af31423fc8ca8651f
Vozudo <jploperatobon@gmail.com>
2022-02-17 00:45:54 -0500

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print hashtags in squares
+ * @size: square size
+ */
+void print_square(int size)
+{
+	int m, n;
+
+	for (m = 0; m < size; m++)
+	{
+		for (n = 0; n < size; n++)
+		{
+			_putchar(35);
+		}
+
+		if (m != size -1)
+		{
+			_putchar(10);
+		}
+	}
+	_putchar(10)
+}
