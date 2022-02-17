27191e16f2b7adc9b48c826c758f39302a642296
Vozudo <jploperatobon@gmail.com>
2022-02-17 16:15:44 -0500

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,2 +1,21 @@
 #include "main.h"
-/
+/**
+ * print_number - print integer
+ * @n: integer
+ */
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar(45)
+		a = -a;
+	}
+
+	if ((a / 10) == 0)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0')
+}
