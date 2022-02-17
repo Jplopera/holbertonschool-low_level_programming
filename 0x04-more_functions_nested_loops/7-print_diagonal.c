498e603e58c9fb9f3b98641921cdbf2eebec7971
Vozudo <jploperatobon@gmail.com>
2022-02-17 00:39:50 -0500

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal n times
+ * @n int variable
+ */
+void print_diagonal(int n)
+{
+	int m, l;
+
+	for (m =0; m < n; m++)
+	{
+		for (l = 0; l < m; l++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+
+		if (m < (n -1))
+		{
+			_putchar(10);
+		}
+	}
+	_putchar(10);
+}
