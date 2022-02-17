319c2bd50c23a5b164b247df3cfc62d07068860c
Vozudo <jploperatobon@gmail.com>
2022-02-16 23:59:37 -0500

    if number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - search numbers
+ * @c: int variable
+ * Return: 1 if uppercase 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return; (0);
+	}
+}
