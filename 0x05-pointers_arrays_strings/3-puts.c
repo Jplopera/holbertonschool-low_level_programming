eacfbc617f903abd56dc739c0ef355f2db3aa90b
Vozudo <jploperatobon@gmail.com>
2022-02-21 22:18:35 -0500

    solving 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: pointer
+ */
+void _puts(char *str);
+{
+	int r;
+
+	for (r = 0; s[r] != 00; r++)
+	{
+		_putchar(str[r]);
+	}
+	_putchar(10)
+}
