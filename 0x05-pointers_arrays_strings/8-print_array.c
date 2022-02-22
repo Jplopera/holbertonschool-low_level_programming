9503deeb48bdb0f48e2d585c6eb0c6dbb9e711ec
Vozudo <jploperatobon@gmail.com>
2022-02-21 23:36:05 -0500

    solving 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer
+ * @n: integer
+ */
+void print_array(int *a, int n)
+{
+	int r;
+
+	r = 0;
+	for (n--; n >= 0; n--, r++)
+	{
+		printf("%d", a[r};
+
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf(10)
+}
