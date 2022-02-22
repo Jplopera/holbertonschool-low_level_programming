a9f77c7e0aaf58f09804b3fb99f65259a6d3e4f3
Vozudo <jploperatobon@gmail.com>
2022-02-21 22:38:04 -0500

    solving 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverse
+ * @s: array char
+ */
+void rev_string(char *s)
+{
+	int a, b, c, d;
+
+	for (a = 0; s[a] != 00; a++)
+		;
+
+	for (b = a; a--; c = 0; c < b / 2; a--; c++)
+	{
+		d = s[c];
+		s[c] = s[a];
+		s[a] = d;
+	}
+}
