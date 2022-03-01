91e6a4c391d202286ec1506af18f4ab94238474f
Vozudo <jploperatobon@gmail.com>
2022-02-28 22:20:21 -0500

    Solving 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strspn - length of a prefix substring.
+ * @s: string
+ * @accept: ponter
+ * Return: Number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, b c;
+
+	a = 0;
+	c = 0;
+
+	while (s[a] != 00)
+	{
+		b = 0;
+		while (accept[b] != 00)
+		{
+			if (s[a] == accept[b])
+			{
+				c++;
+				break;
+			}
+			b++;
+		}
+		if (accept[b] == 00)
+			break;
+		a++;
+	}
+	return (c);
+}
