cd8340feaaf6040beae8430fd4f28ffa4d627394
Vozudo <jploperatobon@gmail.com>
2022-02-28 20:02:58 -0500

    Solving 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with byte
+ * @s: pointer
+ * @b: constant byte
+ * @n: n bytes
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a = 0;
+
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
