0d758219ef87ef05ae8b8d2814d98ef0266f80b3
Vozudo <jploperatobon@gmail.com>
2022-03-04 22:19:22 -0500

    Solving 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print file name
+ * @argc: count
+ * @argv: vector
+ * Return: =
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
