8e9c640800c79db064c531baa1aa17b8dd61dd70
Vozudo <jploperatobon@gmail.com>
2022-03-04 23:00:57 -0500

    Solving 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - add positive number
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != 00; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
