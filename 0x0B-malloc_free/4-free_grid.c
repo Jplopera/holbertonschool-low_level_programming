b0eb33e9f5ca4433e85928bfc8c75890e9095676
Vozudo <jploperatobon@gmail.com>
2022-03-07 23:29:26 -0500

    solving 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: to be free
+ * @height: Height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+		free(grid[x]);
+	free(grig);
+}
