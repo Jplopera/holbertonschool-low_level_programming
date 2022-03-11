9d02d6d3b674c825c0e2449d056cd52567b917bc
Vozudo <jploperatobon@gmail.com>
2022-03-11 15:53:00 -0500

    solvin 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - array memory allocation
+ * @nmemb: number of elements
+ * @size: size of bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *s;
+	unsigned int a;
+
+	if (nmemb <= 0 || size <= 0)
+		return (0);
+
+	s = malloc(nmemb * size);
+
+	if (s == 0)
+		return (0);
+
+	for (a = 0; a < nmemb; a++)
+		s[a] = 0;
+
+	return (s);
+}
