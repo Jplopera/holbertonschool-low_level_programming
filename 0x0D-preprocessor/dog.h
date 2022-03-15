02d35dfb55e8e7c80282dea9ec1eacc9eea87315
Vozudo <jploperatobon@gmail.com>
2022-03-14 23:50:05 -0500

    dog.h

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data structure
+ * @name: Name of the dog
+ * @age: Dog age
+ * @owner: owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif /* DOG_H */
