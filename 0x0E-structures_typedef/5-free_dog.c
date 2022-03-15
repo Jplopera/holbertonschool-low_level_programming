0ceba623a988149d9224eeb02dcdc0585cdee21d
Vozudo <jploperatobon@gmail.com>
2022-03-15 00:13:02 -0500

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * free_dog - free dogs
+ * @d: pointer
+ */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != 0)
 	{
 		free(d->name);
 		free(d->owner);
