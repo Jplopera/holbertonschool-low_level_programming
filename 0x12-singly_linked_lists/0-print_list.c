580b00222958bf5e941aa2c1115310f1e02941d2
Vozudo <jploperatobon@gmail.com>
2022-03-29 23:31:00 -0500

    [0x12]: change count for h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 
-	for (count = 0; count != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
 		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
