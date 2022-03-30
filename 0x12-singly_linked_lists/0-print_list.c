9fa914d7a0658d881ebc4dba4263546dafefed91
Vozudo <jploperatobon@gmail.com>
2022-03-29 23:24:56 -0500

    [0x12]: linked list 0 solve

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: node to be printed
+ * Return: noud counter
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	for (count = 0; count != NULL; count++)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nill)\n");
+		h = h->next;
+	}
+	return (count);
+}
