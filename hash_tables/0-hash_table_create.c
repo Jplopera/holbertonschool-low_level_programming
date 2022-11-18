7ec37894a0f615097c1947abe466cb49c9ef574a
Vozudo <jploperatobon@gmail.com>
2022-11-18 03:43:05 -0500

    betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,16 +8,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
+	hash_table_t *table;
 
-    if (size == 0)
-        return (NULL);
-    table = malloc(sizeof(hash_table_t));
-    if (!table)
-        return (NULL);
-    table->size = size;
-    table->array = malloc(sizeof(table) * size);
-    if (!table->array)
-        return (NULL);
-    return (table);
+	if (size == 0)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(table) * size);
+	if (!table->array)
+		return (NULL);
+	return (table);
 }
